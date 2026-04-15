25fb6fb72134b939586592b01a70cc4b318f826b
linnealindgren <linnealindgren96@gmail.com>
2026-04-15 11:32:58 +0200

    Add BasicFood and extend FoodData
    Add ABasicFood actor (header + cpp) that initializes its FoodData to FFoodData::Basic. Expand FFoodData: make several properties read-only/VisibleDefaultsOnly, add SpeedMultiplier and bGrantsInvisibility fields, and provide static factory helpers (Basic, Special, SpeedBoost, Golden). Add a property redirect in DefaultEngine.ini to map the old SpeedBoost property to the new SpeedMultiplier. Update blueprint and map assets to reflect these data/API changes.

---
+++ Source/ProjectSnake/Private/BasicFood.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicFood.h"
+
+ABasicFood::ABasicFood()
+{
+	FoodData = FFoodData::Basic();
+}
--- Source/ProjectSnake/Public/FoodData.h
+++ Source/ProjectSnake/Public/FoodData.h
@@ -17,12 +17,63 @@ struct FFoodData
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
     EFoodType FoodType = EFoodType::Basic;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
     int32 PointValue = 10;
+    
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+    float SpeedMultiplier = 1.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
     float BenefitDuration = 0.0f;
+    
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+    bool bGrantsInvisibility = false;
+
+
+    static FFoodData Basic()
+    {
+        FFoodData Data;
+        Data.FoodType = EFoodType::Basic;
+        Data.PointValue = 10;
+        Data.SpeedMultiplier = 1.0f;
+        Data.BenefitDuration = 0.0f;
+        Data.bGrantsInvisibility = false;
+        return Data;
+    }
+
+    static FFoodData Special()
+    {
+        FFoodData Data;
+        Data.FoodType = EFoodType::Special;
+        Data.PointValue = 30;
+        Data.SpeedMultiplier = 1.0f;
+        Data.BenefitDuration = 0.0f;
+        Data.bGrantsInvisibility = false;
+        return Data;
+    }
+
+    static FFoodData SpeedBoost()
+    {
+        FFoodData Data;
+        Data.FoodType = EFoodType::SpeedBoost;
+        Data.PointValue = 30;
+        Data.SpeedMultiplier = 1.5f;
+        Data.BenefitDuration = 0.0f;
+        Data.bGrantsInvisibility = false;
+        return Data;
+    }
+
+    static FFoodData Golden()
+    {
+        FFoodData Data;
+        Data.FoodType = EFoodType::Golden;
+        Data.PointValue = 100;
+        Data.SpeedMultiplier = 1.0f;
+        Data.BenefitDuration = 0.0f;
+        Data.bGrantsInvisibility = true;
+        return Data;
+    }
 };
